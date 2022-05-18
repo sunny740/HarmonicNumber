257c59d5455087c5601c32f99e330994af2732c8
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-18 09:24:31 +0530

    [Sunny] Add : That is a Workshop program of Harmonic Number

---
+++ HarmonicProgram/Harmonic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicProgram
+{
+    public class Harmonic
+    {
+        public void Number()
+        {
+            int num = 0;
+            double Result = 0.0;
+            Console.Write("Input Your Number: ");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                Result += 1 / (float)i;
+                Console.Write("1/{0} + ", 1);
+            }
+            Console.WriteLine($"= {Result}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HarmonicProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HarmonicProgram
+{
+    public class program
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Harmonic Number:");
+            bool end = true;
+            Console.WriteLine("1. Harmonic Number\n");
+            while (end == true)
+            {
+                Console.WriteLine("Choose Harmonic:");
+                int option = Convert.ToInt32(Console.ReadLine();
+                switch (option)
+                {
+                    case 1:
+                        Harmonic num = new Harmonic();
+                        num.Number();
+                        break;
+                    default:
+                        Console.WriteLine("Choose Correct Number");
+                        break;
+                }
+            }
+        }
+    }
+}
